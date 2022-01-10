d5b5e6b62b3c9bd785c0ca93deaf176e8271c957
andrelake <andre.lake@hotmail.com>
2022-01-10 17:36:00 -0300

    cria dominio Categoria; atualiza CategoriaResource

---
+++ src/main/java/com/andre/petshop/domain/Categoria.java
@@ -0,0 +1,47 @@
+package com.andre.petshop.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Categoria implements Serializable {
+
+    private Integer id;
+    private String name;
+
+    public Categoria() {
+    }
+
+    public Categoria(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Categoria categoria = (Categoria) o;
+        return Objects.equals(id, categoria.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
--- src/main/java/com/andre/petshop/resources/CategoriaResource.java
+++ src/main/java/com/andre/petshop/resources/CategoriaResource.java
@@ -1,15 +1,29 @@
 package com.andre.petshop.resources;
 
+import com.andre.petshop.domain.Categoria;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
     @GetMapping
-    public String mostrar() {
-        return "teste";
+    public List<Categoria> mostrar() {
+
+        Categoria cat1 = new Categoria(1, "Alimento");
+        Categoria cat2 = new Categoria(2, "Cosmético");
+        Categoria cat3 = new Categoria(3, "Remédio");
+
+        List<Categoria> lista = new ArrayList<>();
+        lista.add(cat1);
+        lista.add(cat2);
+        lista.add(cat3);
+
+        return lista;
     }
 }
